6289582e19a152dda13a8c2182719b982a05afc4
Jenil <jenilmistryhq@users.noreply.github.com>
2025-12-01 11:25:05 +0000

    Stop tracking core and config

--- config/__init__.py
+++
@@ -1,4 +0,0 @@
-__version__ = "0.0.1"
-
-from .middleware import SanitizerMiddleware
-from .sanitizer import sanitize
--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from core import views
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("echo-json/", views.echo_json),
-    path("form-url/", views.form_view),
-]
